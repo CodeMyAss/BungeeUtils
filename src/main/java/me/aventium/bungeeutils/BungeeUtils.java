61e48da1e8a713233348ed199cfab3a1d13f1f60
Ryan <rbrickproductions123@gmail.com>
2014-09-11 15:39:01 -0500

    Update BungeeUtils.java

--- src/main/java/me/aventium/bungeeutils/BungeeUtils.java
+++ src/main/java/me/aventium/bungeeutils/BungeeUtils.java
@@ -6,11 +6,16 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class BungeeUtils extends Plugin {
+    
+    HashMap<String, String> messages;
 
     public void onEnable() {
         // Register commands
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new Message());
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new Reply());
     }
-
+    
+    public HashMap<String, String> getMessages() {
+      return messages;
+    }
 }
