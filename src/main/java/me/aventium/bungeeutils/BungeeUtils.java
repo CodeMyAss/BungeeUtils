ad9a5b7ed2b8074cef39835acba362a71449c24d
Cameron Andersson <aventiumhd@gmail.com>
2014-09-11 13:49:30 -0400

    Jenkins!

--- src/main/java/me/aventium/bungeeutils/BungeeUtils.java
+++ src/main/java/me/aventium/bungeeutils/BungeeUtils.java
@@ -8,6 +8,7 @@ import net.md_5.bungee.api.plugin.Plugin;
 public class BungeeUtils extends Plugin {
 
     public void onEnable() {
+        // Register commands
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new Message());
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new Reply());
     }
