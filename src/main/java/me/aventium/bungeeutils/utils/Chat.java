a47c99d92c44a63f0790c8432aec2329e9d41d97
Ryan <rbrickproductions123@gmail.com>
2014-09-11 15:36:17 -0500

    Update Chat.java

--- src/main/java/me/aventium/bungeeutils/utils/Chat.java
+++ src/main/java/me/aventium/bungeeutils/utils/Chat.java
@@ -9,10 +9,14 @@ import java.util.HashMap;
 
 public class Chat {
 
-    public static HashMap<String, String> messages = new HashMap<>();
+    public HashMap<String, String> messages = new HashMap<>();
 
     public static void sendMessage(CommandSender arg0, String arg1) {
         arg0.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', arg1)));
+    }
+    
+    public static HashMap<String, String> getMessages() {
+        return messages;
     }
 
 }
